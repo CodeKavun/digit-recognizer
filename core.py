fb3ffa57a34c71458dae2b7de80e833180bc4315
PanDmytryk <dimacapny22@gmail.com>
2025-10-26 13:45:43 +0100

    Use mnist

--- core.py
+++ core.py
@@ -1,23 +1,26 @@
 import os
 
 from PIL import ImageOps
-from tensorflow.python.keras.layers import Flatten, Dense
-from tensorflow.python.keras.models import load_model, Sequential
-from tensorflow.python.keras.optimizer_v2.adam import Adam
+from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras.models import load_model, Sequential
+from tensorflow.keras.optimizers import Adam
 
 import numpy as np
 
 
 MODEL_PATH = 'digits.h5'
+mode = 'train'
 
 if os.path.exists(MODEL_PATH):
+    mode = 'predict'
     print('Loading existing model...')
     model = load_model(MODEL_PATH)
 else:
+    mode = 'train'
     print('Creating new model')
     model = Sequential([
         Flatten(input_shape=(28, 28)),
-        Dense(128, activation='relu'),
+        Dense(256, activation='relu'),
         Dense(10, activation='softmax')
     ])
     model.compile(
@@ -30,10 +33,10 @@ else:
 train_images, train_labels = [], []
 
 
-def train(img, number_value, train_count) -> bool:
+def train(img, number_value, train_count):
     global train_images, train_labels
 
-    img = ImageOps.invert(img)
+    img=ImageOps.invert(img)
     img_array = np.array(img) / 255.0
 
     train_images.append(img_array)
@@ -50,9 +53,87 @@ def train(img, number_value, train_count) -> bool:
 
 def predict(img):
     img = ImageOps.invert(img)
-    img_array = np.array(img) / 255.0
+    img_array = np.array(img).astype('float32') / 255.0
+    img_array = img_array.reshape(1, 784)
 
     prediction = model.predict(np.expand_dims(img_array, axis=0))
     result = np.argmax(prediction)
 
     return result
+
+
+def mnist():
+    global model, mode, train_images, train_labels
+
+    import random
+    import matplotlib.pyplot as plt
+    import matplotlib
+    import tensorflow as tf
+
+    from tensorflow.keras import layers
+    from tensorflow.keras.models import Sequential
+    from tensorflow.keras.layers import Dense, Dropout
+    from tensorflow.keras.datasets import mnist
+    from tensorflow.keras.utils import to_categorical
+
+    from matplotlib.ticker import (MultipleLocator, FormatStrFormatter)
+
+
+    plt.rcParams['axes.titlepad'] = 16
+    plt.rcParams['axes.labelsize'] = 14
+    plt.rcParams['image.cmap'] = 'gray'
+
+    SEED_VALUE = 42
+
+    random.seed(SEED_VALUE)
+    np.random.seed(SEED_VALUE)
+    tf.random.set_seed(SEED_VALUE)
+
+    (x_train_all, y_train_all), (x_test, y_test) = mnist.load_data()
+
+    x_valid = x_train_all[:10000]
+    x_train = x_train_all[10000:]
+
+    y_valid = y_train_all[:10000]
+    y_train = y_train_all[10000:]
+
+    plt.figure(figsize=(6, 5))
+    plt.axis(True)
+    plt.imshow(x_train[11], cmap='gray')
+    plt.subplots_adjust(wspace=0.2, hspace=0.2)
+    plt.show()
+
+    x_train = x_train.reshape((x_train.shape[0], 28 * 28))
+    x_train = x_train.astype('float32') / 255.0
+
+    x_test = x_test.reshape((x_test.shape[0], 28 * 28))
+    x_test = x_test.astype('float32') / 255.0
+
+    y_train = to_categorical(y_train)
+    y_valid = to_categorical(y_valid)
+    y_test = to_categorical(y_test)
+
+    model = tf.keras.Sequential()
+    model.add(Dense(128, activation='relu', input_shape=(x_train.shape[1],)))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(10, activation='softmax'))
+
+    model.summary()
+
+    model.compile(
+        optimizer='rmsprop',
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    training_result = model.fit(
+        x_train, y_train,
+        epochs=21,
+        batch_size=64,
+        validation_data=(x_valid, y_valid)
+    )
+
+    print(training_result)
+
+    model.save(MODEL_PATH)
+    mode = 'predict'
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 
 from PIL import Image, ImageDraw
@@ -6,8 +7,8 @@ from PIL import Image, ImageDraw
 import core
 
 # === Глобальні змінні ===
-mode = "train"
-TRAIN_COUNT = 6
+tmp_count = 0
+TRAIN_COUNT = 1
 
 # === INTERFACE ===
 # === ROOT ===
@@ -19,12 +20,15 @@ root.protocol("WM_DELETE_WINDOW", lambda: 0)
 # === CONTROLS ===
 canvas = tk.Canvas(root)
 status_label = tk.Label(root, text="Готово ✅")
+train_count = tk.Entry(root)
+train_count.insert(0, str(TRAIN_COUNT))
 entry = tk.Entry(root)
 # === Drawing canvas ===
 image = Image.new("L", (280, 280), "white")
 draw = ImageDraw.Draw(image)
 # === BUTTONS ===
-mode_btn = tk.Button(root, text="TRAIN")
+train_mnist = tk.Button(root, text="Train Mnist")
+mode_btn = tk.Button(root, text=f"{core.mode.upper()}")
 ok_btn = tk.Button(root, text="OK")
 clear_btn = tk.Button(root, text="Очистити")
 quit_btn = tk.Button(root, text="Вихід")
@@ -42,38 +46,56 @@ def paint(event):
 def clear_canvas():
     canvas.delete("all")
     draw.rectangle((0, 0, 280, 280), fill="white")
+    entry.delete(0, tk.END)
 
 
 # === Перемикання режиму ===
 def toggle_mode():
-    global mode
-    mode = "predict" if mode == "train" else "train"
-    mode_btn.config(text=f"{mode.upper()}")
+    # global mode
+    if os.path.exists(core.MODEL_PATH):
+        os.remove(core.MODEL_PATH)
+        print("Model deleted!")
+    core.mode = "predict" if core.mode == "train" else "train"
+    mode_btn.config(text=f"{core.mode.upper()}")
     clear_canvas()
 
 
 # === Обробка зображення ===
 def process_image_listener():
+    global TRAIN_COUNT, tmp_count
     img = image.resize((28, 28))
-    if mode == "train":
+    if core.mode == "train":
+        TRAIN_COUNT = int(train_count.get())
         if entry.get().isdigit():
             label_value = int(entry.get())
-            status_label.config(text="🔄 Навчаюся...")
+            tmp_count += 1
+            status_label.config(text=f"🔄 Додавання даних... {tmp_count}/{TRAIN_COUNT}")
             root.update()
             # TRAIN
             trained = core.train(img, label_value, TRAIN_COUNT)
             if trained:
                 status_label.config(text="✅ Модель оновлена і збережена")
+                core.mode = "predict"
+                mode_btn.config(text=f"{core.mode.upper()}")
+                clear_canvas()
                 root.update()
-        entry.delete(0, tk.END)
     else:  # predict
+        tmp_count = 0
         result = core.predict(img)
         entry.delete(0, tk.END)
         entry.insert(0, f"{result}")
-    clear_canvas()
+    canvas.delete("all")
+    draw.rectangle((0, 0, 280, 280), fill="white")
+
+def train_mnist_listener():
+    core.mnist()
+    mode_btn.config(text=f"{core.mode.upper()}")
+    root.update()
+
 
 
 # === LISTENERS ===
+train_mnist.config(command=train_mnist_listener)
 canvas.bind("<B1-Motion>", paint)
 mode_btn.config(command=toggle_mode)
 ok_btn.config(command=process_image_listener)
@@ -84,7 +106,9 @@ WIDTH = 12
 FONT = ("serif", 12, "normal")
 FONT_LARGE = ("serif", 25)
 #
+train_mnist.config(bg="#DCDCDC", width=WIDTH, font=FONT)
 canvas.config(width=280, height=280, bg="white")
+train_count.config(width=WIDTH, font=FONT_LARGE, justify=tk.CENTER)
 status_label.config(fg="green")
 entry.config(width=WIDTH, font=FONT_LARGE, justify=tk.CENTER)
 mode_btn.config(bg="lightblue", font=FONT, width=WIDTH)
@@ -92,13 +116,15 @@ ok_btn.config(bg="lightgreen", font=FONT, width=WIDTH)
 clear_btn.config(bg="#DCDCDC", font=FONT, width=WIDTH)
 quit_btn.config(bg="salmon", font=FONT, width=WIDTH)
 # === POSITION ===
-quit_btn.grid(row=0, column=0)
-mode_btn.grid(row=0, column=1)
-status_label.grid(row=1, column=0, columnspan=2, pady=10)
-canvas.grid(row=2, column=0, columnspan=2)
-entry.grid(row=3, column=0, pady=15, columnspan=2)
-clear_btn.grid(row=4, column=0)
-ok_btn.grid(row=4, column=1)
+train_mnist.grid(row=0, column=0, columnspan=2)
+quit_btn.grid(row=1, column=0)
+mode_btn.grid(row=1, column=1)
+train_count.grid(row=2, column=0, columnspan=2, pady=10)
+status_label.grid(row=3, column=0, columnspan=2, pady=10)
+canvas.grid(row=4, column=0, columnspan=2)
+entry.grid(row=5, column=0, pady=15, columnspan=2)
+clear_btn.grid(row=6, column=0)
+ok_btn.grid(row=6, column=1)
 # === WINDOW POSITION ===
 root.update_idletasks()
 window_width = root.winfo_width()
